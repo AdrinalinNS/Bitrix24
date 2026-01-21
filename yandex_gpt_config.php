9acbd4def9dd4b4e0611a1ea221b302461fcdf11
AdrinalinNS <adrinalinns@gmail.com>
2026-01-21 16:39:37 +1000

    Update yandex_gpt_config.php
    LMM

--- yandex_gpt_config.php
+++ yandex_gpt_config.php
@@ -1,5 +1,5 @@
 <?php
 // Настройки Yandex GPT
-$yandexApiUrl = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'; // запрос в яндекс, можно выбрать любую модель из доступных в Яндекс
+$yandexApiUrl = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'; // выбор модели LLM в яндекс, можно выбрать любую модель из доступных в Яндекс
 $iamToken = 'your_yandex_gpt_api_key'; // замените на ваш IAM-токен
 $folderId = 'your_yandex_cloud_folder_id';  // замените на ID каталога
